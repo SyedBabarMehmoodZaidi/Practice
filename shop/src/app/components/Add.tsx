3f7eddac794fa2c31888c8389a4c34d37b98d260
Babar Mehmood <babarzaidi1122@gmail.com>
2025-01-08 23:34:29 +0500

    commit

---
+++ shop/src/app/components/Add.tsx
@@ -0,0 +1,43 @@
+"use client"
+
+
+import React, { useState } from 'react'
+
+const Add = () => {
+
+    const [quantity,setQuantity] = useState(1);
+
+     // Temporary
+    const stock = 4 
+
+    const handleQuantity = (type: "i" | "d") => {
+        if(type==="d" && quantity > 1){
+            setQuantity((prev)=> prev - 1);
+        }
+        if(type==="i" && quantity < stock){
+            setQuantity((prev)=> prev + 1);
+        }
+    }
+
+  return (
+    <div className='flex flex-col gap-4'>
+        <h4 className='font-medium'>Choose a Qunatity</h4>
+        <div className="flex justify-between">
+            <div className="flex items-center gap-4">
+            <div className="bg-gray-100 py-2 px-4 rounded-3xl flex justify-between items-center w-32">
+                <button className='cursor-pointer text-xl' onClick={()=>handleQuantity("d")}>-</button>
+                {quantity}
+                <button className='cursor-pointer text-xl' onClick={()=>handleQuantity("i")}>+</button>
+            </div>
+            <div className="text-xs">Only <span className='text-orange-500'>4 items</span> left! <br /> {"Don't"} miss it</div>
+            </div>
+        </div>
+        
+        <button className='w-36 text-sm rounded-3xl ring-1 ring-apnaghar text-apnaghar py-2 px-4 hover:bg-red-500 hover:text-white disabled:cursor-not-allowed disabled:bg-pink-200 disabled:text-white disabled:ring-none'>
+            Add to Cart</button>
+      
+    </div>
+  )
+}
+
+export default Add
--- shop/src/app/page.tsx
+++ shop/src/app/page.tsx
@@ -1,10 +1,12 @@
 import Carousel from "./components/carousel";
+import Products from "./products/page";
 
 
 export default function Home() {
   return (
    <>
    <Carousel />
+   <Products />
     </>
   );
 }
